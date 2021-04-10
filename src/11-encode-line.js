4add5ebcb81105d8ebbaa4e1580d8f03768ca542
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2021-04-10 17:31:06 +0400

    feat:implement encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const result = [];
+  let count = 0;
+  while (arr.length) {
+    count = 1;
+    const ch = arr.splice(0, 1)[0];
+    while (ch === arr[0]) {
+      count++;
+      arr.splice(0, 1);
+    }
+    result.push(((count > 1) ? `${count}${ch}` : ch));
+  }
+  return result.join('');
 }
 
 module.exports = encodeLine;
