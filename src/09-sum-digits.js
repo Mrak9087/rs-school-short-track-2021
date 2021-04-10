23cf1b4912cff8a32fdffdff6a88ecf3d40a0304
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2021-04-10 15:16:01 +0400

    feat:implement sum-digits

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,13 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  let result = n;
+  while (result > 9) {
+    const tmpArr = (`${result}`).split('').map(Number);
+    result = tmpArr.reduce((sum, item) => sum + item, 0);
+  }
+  return result;
 }
 
 module.exports = getSumOfDigits;
